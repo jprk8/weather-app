2f597f99e4a0b9ac5d41f7d3630781f4263fafe0
John Park <176120986+jprk8@users.noreply.github.com>
2024-10-10 23:44:34 -0700

    Format with prettier

--- src/display-controller.js
+++ src/display-controller.js
@@ -11,16 +11,16 @@ import snowIcon from './icon/snow.svg';
 import windyIcon from './icon/windy.svg';
 
 const weatherIcons = {
-    clearDay: clearDayIcon,
-    clearNight: clearNightIcon,
-    cloudy: cloudyIcon,
-    fog: fogIcon,
-    partlyCloudyDay: partlyCloudyDayIcon,
-    partlyCloudyNight: partlyCloudyNightIcon,
-    rain: rainIcon,
-    snow: snowIcon,
-    windy: windyIcon
-}
+  clearDay: clearDayIcon,
+  clearNight: clearNightIcon,
+  cloudy: cloudyIcon,
+  fog: fogIcon,
+  partlyCloudyDay: partlyCloudyDayIcon,
+  partlyCloudyNight: partlyCloudyNightIcon,
+  rain: rainIcon,
+  snow: snowIcon,
+  windy: windyIcon,
+};
 
 const header = document.querySelector('.header');
 const weatherContainer = document.querySelector('.weather-container');
@@ -38,89 +38,89 @@ const humidity = document.querySelector('.humidity');
 const uv = document.querySelector('.uv');
 
 function showWeather(weather, fahrenheit) {
-    header.style.cssText = 'padding: 14px;';
-    resolvedLocation.textContent = weather.location;
-    const today = new Date(weather.forecast[0].datetime.replace('-', '/'));
-    dateTime.textContent = format(today, 'PPPP');
-    description.textContent = weather.description;
-    weatherContainer.style.cssText = 'padding: 20px';
-    currentIcon.src = weatherIcons[camelize(weather.icon)];
-    currentIcon.style.display = 'block';
+  header.style.cssText = 'padding: 14px;';
+  resolvedLocation.textContent = weather.location;
+  const today = new Date(weather.forecast[0].datetime.replace('-', '/'));
+  dateTime.textContent = format(today, 'PPPP');
+  description.textContent = weather.description;
+  weatherContainer.style.cssText = 'padding: 20px';
+  currentIcon.src = weatherIcons[camelize(weather.icon)];
+  currentIcon.style.display = 'block';
 
-    if (weather.icon.includes('day')) {
-        weatherContainer.style.backgroundColor = 'rgb(42, 146, 194)';
-    } else if (weather.icon.includes('night')) {
-        weatherContainer.style.backgroundColor = 'rgb(13, 28, 63)';
-    } else {
-        weatherContainer.style.backgroundColor = 'rgb(102, 107, 121)';
-    }
+  if (weather.icon.includes('day')) {
+    weatherContainer.style.backgroundColor = 'rgb(42, 146, 194)';
+  } else if (weather.icon.includes('night')) {
+    weatherContainer.style.backgroundColor = 'rgb(13, 28, 63)';
+  } else {
+    weatherContainer.style.backgroundColor = 'rgb(102, 107, 121)';
+  }
 
-    if (fahrenheit ) {
-        actualTemp.textContent = `${Math.round(weather.temp)}℉`;
-        feelslike.textContent = `Feels like ${Math.round(weather.feelslike)}℉`;
-        maxMin.textContent = `H: ${Math.round(weather.forecast[0].max)}℉ / L: ${Math.round(weather.forecast[0].min)}℉`;
-    } else {
-        actualTemp.textContent = `${Math.round(weather.tempC)}℃`;
-        feelslike.textContent = `Feels like ${Math.round(weather.feelslikeC)}℃`;
-        maxMin.textContent = `H: ${Math.round(weather.forecast[0].maxC)}℃ / L: ${Math.round(weather.forecast[0].minC)}℃`;
-    }
+  if (fahrenheit) {
+    actualTemp.textContent = `${Math.round(weather.temp)}℉`;
+    feelslike.textContent = `Feels like ${Math.round(weather.feelslike)}℉`;
+    maxMin.textContent = `H: ${Math.round(weather.forecast[0].max)}℉ / L: ${Math.round(weather.forecast[0].min)}℉`;
+  } else {
+    actualTemp.textContent = `${Math.round(weather.tempC)}℃`;
+    feelslike.textContent = `Feels like ${Math.round(weather.feelslikeC)}℃`;
+    maxMin.textContent = `H: ${Math.round(weather.forecast[0].maxC)}℃ / L: ${Math.round(weather.forecast[0].minC)}℃`;
+  }
 
-    condition.textContent = `Condition: ${weather.condition}`;
-    conditions.style.cssText = 'padding: 10px 20px';
-    precip.textContent = `Precipitation: ${weather.precip}%`;
-    humidity.textContent = `Humidity: ${weather.humidity}%`;
-    uv.textContent = `UV Index: ${weather.uv}.0`;
+  condition.textContent = `Condition: ${weather.condition}`;
+  conditions.style.cssText = 'padding: 10px 20px';
+  precip.textContent = `Precipitation: ${weather.precip}%`;
+  humidity.textContent = `Humidity: ${weather.humidity}%`;
+  uv.textContent = `UV Index: ${weather.uv}.0`;
 }
 
 const forecast = document.querySelector('.forecast');
 
 function showForecast(weather, fahrenheit) {
-    const removeForecast = document.querySelectorAll('.forecast > *');
-    removeForecast.forEach((item) => {
-        forecast.removeChild(item);
-    });
-    const title = document.createElement('div');
-    title.className = 'forecast-title';
-    title.textContent = '7-day Forecast';
-    forecast.appendChild(title);
-    forecast.style.display = 'block';
-    for (let i = 1; i < 8; i++) {
-        const row = document.createElement('div');
-        const date = document.createElement('div');
-        const icon = document.createElement('img');
-        const condition = document.createElement('div');
-        const temp = document.createElement('div');
-        row.className = 'forecast-row';
-        date.className = 'forecast-date';
-        condition.className = 'forecast-condition';
-        temp.className = 'forecast-temp';
-        const shortDate = new Date(weather.forecast[i].datetime.replace('-', '/'));
-        date.textContent = format(shortDate, 'MM/dd EEE');
-        icon.src = weatherIcons[camelize(weather.forecast[i].icon)];
-        condition.textContent = weather.forecast[i].condition;
-        if (fahrenheit) {
-            const max = Math.round(weather.forecast[i].max);
-            const min = Math.round(weather.forecast[i].min);
-            temp.textContent = `H: ${max}℉ / L: ${min}℉`;
-        } else {
-            const max = Math.round(weather.forecast[i].maxC);
-            const min = Math.round(weather.forecast[i].minC);
-            temp.textContent = `H: ${max}℃ / L: ${min}℃`;
-        }
-
-        row.appendChild(date);
-        row.appendChild(icon);
-        row.appendChild(condition);
-        row.appendChild(temp);
-        forecast.appendChild(row);
+  const removeForecast = document.querySelectorAll('.forecast > *');
+  removeForecast.forEach((item) => {
+    forecast.removeChild(item);
+  });
+  const title = document.createElement('div');
+  title.className = 'forecast-title';
+  title.textContent = '7-day Forecast';
+  forecast.appendChild(title);
+  forecast.style.display = 'block';
+  for (let i = 1; i < 8; i++) {
+    const row = document.createElement('div');
+    const date = document.createElement('div');
+    const icon = document.createElement('img');
+    const condition = document.createElement('div');
+    const temp = document.createElement('div');
+    row.className = 'forecast-row';
+    date.className = 'forecast-date';
+    condition.className = 'forecast-condition';
+    temp.className = 'forecast-temp';
+    const shortDate = new Date(weather.forecast[i].datetime.replace('-', '/'));
+    date.textContent = format(shortDate, 'MM/dd EEE');
+    icon.src = weatherIcons[camelize(weather.forecast[i].icon)];
+    condition.textContent = weather.forecast[i].condition;
+    if (fahrenheit) {
+      const max = Math.round(weather.forecast[i].max);
+      const min = Math.round(weather.forecast[i].min);
+      temp.textContent = `H: ${max}℉ / L: ${min}℉`;
+    } else {
+      const max = Math.round(weather.forecast[i].maxC);
+      const min = Math.round(weather.forecast[i].minC);
+      temp.textContent = `H: ${max}℃ / L: ${min}℃`;
     }
+
+    row.appendChild(date);
+    row.appendChild(icon);
+    row.appendChild(condition);
+    row.appendChild(temp);
+    forecast.appendChild(row);
+  }
 }
 
 function camelize(str) {
-    return str
-        .split('-')
-        .map((word, index) => 
-            index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-        )
-        .join('');
+  return str
+    .split('-')
+    .map((word, index) =>
+      index === 0 ? word : word[0].toUpperCase() + word.slice(1),
+    )
+    .join('');
 }
--- src/weather.js
+++ src/weather.js
@@ -4,50 +4,50 @@ export { getWeather };
 const apiKey = 'R7AFFHHQKGCWNZBKSJW2ZCZME';
 
 async function getWeather(location) {
-    try {
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
-        const response = await fetch(url, { mode: 'cors' });
-        if (!response.ok) {
-            throw new Error(response.status);
-        }
-        const data = await response.json();
-        // grab the desired forecast array from the day array
-        const forecast = data.days.map(day => ({
-            datetime: day.datetime,
-            temp: day.temp,
-            tempC: convertToCelsius(day.temp),
-            max: day.tempmax,
-            maxC: convertToCelsius(day.tempmax),
-            min: day.tempmin,
-            minC: convertToCelsius(day.tempmin),
-            condition: day.conditions,
-            precip: day.precip,
-            humidity: day.humidity,
-            icon: day.icon,
-        }));
-        const weather = {
-            location: data.resolvedAddress,
-            temp: data.currentConditions.temp,
-            tempC: convertToCelsius(data.currentConditions.temp),
-            feelslike: data.currentConditions.feelslike,
-            feelslikeC: convertToCelsius(data.currentConditions.feelslike),
-            condition: data.currentConditions.conditions,
-            description: data.description,
-            uv: data.currentConditions.uvindex,
-            precip: data.currentConditions.precipprob,
-            humidity: data.currentConditions.humidity,
-            forecast: forecast,
-            icon: data.currentConditions.icon
-        };
-        console.log(data);
-        console.log(weather);
-        return weather;
-    } catch (error) {
-        console.log(error);
+  try {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
+    const response = await fetch(url, { mode: 'cors' });
+    if (!response.ok) {
+      throw new Error(response.status);
     }
+    const data = await response.json();
+    // grab the desired forecast array from the day array
+    const forecast = data.days.map((day) => ({
+      datetime: day.datetime,
+      temp: day.temp,
+      tempC: convertToCelsius(day.temp),
+      max: day.tempmax,
+      maxC: convertToCelsius(day.tempmax),
+      min: day.tempmin,
+      minC: convertToCelsius(day.tempmin),
+      condition: day.conditions,
+      precip: day.precip,
+      humidity: day.humidity,
+      icon: day.icon,
+    }));
+    const weather = {
+      location: data.resolvedAddress,
+      temp: data.currentConditions.temp,
+      tempC: convertToCelsius(data.currentConditions.temp),
+      feelslike: data.currentConditions.feelslike,
+      feelslikeC: convertToCelsius(data.currentConditions.feelslike),
+      condition: data.currentConditions.conditions,
+      description: data.description,
+      uv: data.currentConditions.uvindex,
+      precip: data.currentConditions.precipprob,
+      humidity: data.currentConditions.humidity,
+      forecast: forecast,
+      icon: data.currentConditions.icon,
+    };
+    console.log(data);
+    console.log(weather);
+    return weather;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function convertToCelsius(tempF) {
-    const celsius = (tempF - 32) * 5/9;
-    return Math.round(celsius);
+  const celsius = ((tempF - 32) * 5) / 9;
+  return Math.round(celsius);
 }
